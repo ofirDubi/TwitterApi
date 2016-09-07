1d93efbc95c97af05dff38e38f3917d1710e0ef0
ofirDubi <ofir123dubi@gmail.com>
2016-09-07 08:06:17 +0300

    Delete try2.py

--- try2.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuthHandler( "vkc9rhetQvsmKxIdsLICGVUJt", "aPuChtFao446UcECcwH7UeYoWU27wyLLH5WYOiVXPrSrP2adlw")
-auth.set_access_token("708405043448442880-lsv6kLnJYbDraL0PhvNNRRxaYPErrEX", "NU8Mv6d3wfbiqqxGNgKuuU9pEMleNTAeJzxNCDwZv7mX1")
-
-api = tweepy.API(auth)
-fashion_image_twitts = api.search("fashion filter:images")
-print fashion_image_twitts[0].entities
